646305654d508f8cb845d4e12293c093f0b7dd60
trainningbrayang <training.brayang.com@gmail.com>
2022-02-08 20:15:27 +0700

    + change index

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-console.log("helo world");
-console.log("helo world");
-console.log("helo world");
+var a = 1.3 + 0.7;
+
+if (a == '2') {
+    let b = 2;
+    console.log('log: ' + (a + b));
+}
+else {
+    console.log("log faile : ");
+}
+
+var array = [1, 2, 3, 4, 5];
+// array.forEach((e) => {
+//     console.log(e);
+// });
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+}
